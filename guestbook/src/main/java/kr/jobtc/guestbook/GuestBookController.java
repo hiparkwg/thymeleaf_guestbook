889f2c10125ab4112cd7be7d42889407bb013cf1
hiparkwg <hiparkwg@gmail.com>
2024-05-20 23:53:01 +0900

    init

---
+++ guestbook/src/main/java/kr/jobtc/guestbook/GuestBookController.java
@@ -0,0 +1,17 @@
+package kr.jobtc.guestbook;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+@RestController
+public class GuestBookController {
+    
+    @RequestMapping(path="/")
+    public ModelAndView index(){
+        ModelAndView mv = new ModelAndView();
+        mv.setViewName("index");
+        return mv;
+    }
+
+}
---
+++ guestbook/src/main/java/kr/jobtc/guestbook/GuestBookVo.java
@@ -0,0 +1,12 @@
+package kr.jobtc.guestbook;
+
+import lombok.Data;
+
+@Data
+public class GuestBookVo {
+    int sno;
+    String id;
+    String doc;
+    String nal;
+    String pwd;
+}
